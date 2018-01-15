485e18cd7abf58d4cee9549d888dee60f4e7c9b9
Sean Cross <sean@xobs.io>
2018-01-15 17:38:49 +0800

    pos2csv: rework completely
    Change a lot from the previous code.  Get something the factory will
    accept.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- pos2csv.js
+++ pos2csv.js
@@ -1,602 +1,83 @@
-if(process.argv.length !== 4){
-	console.log("Check Command Arguments...");
-	console.log("Usage: node pos2csv.js input.pos output.csv");
-	process.exit(1);
+if (process.argv.length !== 4) {
+    console.log("Check Command Arguments...");
+    console.log("Usage: node pos2csv.js input.pos output.csv");
+    process.exit(1);
 }
 
-var input=process.argv[2];
-var output=process.argv[3];
+var input = process.argv[2];
+var output = process.argv[3];
 
-var outputString = '"Designator","NozzleNum","StackNum","Mid X","Mid Y","Rotation","Height","Speed","Vision","Pressure","Explanation"\r\n';
-outputString += '""\r\n';
+//var outputString = '"Designator","NozzleNum","StackNum","Mid X","Mid Y","Rotation","Height","Speed","Vision","Pressure","Explanation"\r\n';
+var outputString = 'Designator,Footprint,Mid X,Mid Y,Ref X,Ref Y,Pad X,Pad Y,Layer,Rotation,Comment\r\n';
+outputString += ',,,,,,,,,,\r\n';
 
 var outputFooter = "";
 
 
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(input)
+    input: require('fs').createReadStream(input)
 });
 
-lineReader.on('line', function (line) {
-	outputString += processLine(line);
+lineReader.on('line', function(line) {
+    outputString += processLine(line);
 });
 
-lineReader.on('close', function (){
-	outputString += outputFooterdata();
-
-	var fs = require('fs');
-	fs.writeFile(output, outputString, function(err) {
-		if(err) {
-			return console.log(err);
-		}
-	}); 
-	//console.log(outputString);
+lineReader.on('close', function() {
+    var fs = require('fs');
+    fs.writeFile(output, outputString, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+    //console.log(outputString);
 });
 
 
-function processLine(line){
-	if(line.lastIndexOf('#', 0) !== 0){
-		while(line.indexOf("  ") > -1){
-			line = line.replace("  ", " ");
-		}		
-		return formatCSV(line.split(" "))  + "\r\n";
-	} else 
-		return '';
+function processLine(line) {
+    if (line.lastIndexOf('#', 0) !== 0) {
+        while (line.indexOf("  ") > -1) {
+            line = line.replace("  ", " ");
+        }
+        return formatCSV(line.split(" ")) + "\r\n";
+    } else
+        return '';
 }
 
-function formatCSV(details){
-	var columnIndex = {
-		Ref		: 0,
-		Val		: 1,
-		Package	: 2,
-		PosX	: 3,
-		PosY	: 4,
-		Rot		: 5,
-		Side	: 6
-		
-	};
-	
-	var result = "";
-	result += '"'+details[columnIndex.Ref] + '",'; 		// Designator
-	result += '"1",';									// NozzleNum
-	result += '"L1",';									// StackNum
-	result += '"'+details[columnIndex.PosX] + '",';		// Mid X
-	result += '"'+details[columnIndex.PosY] + '",';		// Mid Y
-	result += '"'+details[columnIndex.Rot] + '",';		// Rotation
-	result += '"0.00",';								// Height
-	result += '"100",';									// Speed
-	result += '"None",';								// Vision
-	result += '"True",';								// Pressure
-	result += '"'+details[columnIndex.Val] + '"';		// Explanation
+function formatCSV(details) {
+    var xOffset = -17.007257;
+    var yOffset = 28.951274;
+    var columnIndex = {
+        Ref: 0,
+        Val: 1,
+        Package: 2,
+        PosX: 3,
+        PosY: 4,
+        Rot: 5,
+        Side: 6
+    };
 
+    details[columnIndex.PosX] = parseFloat(details[columnIndex.PosX]);
+    details[columnIndex.PosY] = parseFloat(details[columnIndex.PosY]);
+    details[columnIndex.Rot] = parseFloat(details[columnIndex.Rot]);
 
-	return result;
+    details[columnIndex.PosX] += xOffset;
+    details[columnIndex.PosY] += yOffset;
+
+    details[columnIndex.PosX] = Math.round(details[columnIndex.PosX] * 1000) / 1000;
+    details[columnIndex.PosY] = Math.round(details[columnIndex.PosY] * 1000) / 1000;
+
+    var result = "";
+    result += details[columnIndex.Ref] + ','; // Designator
+    result += details[columnIndex.Package] + ','; // Footprint
+    result += details[columnIndex.PosX] + 'mm,'; // Mid X
+    result += details[columnIndex.PosY] + 'mm,'; // Mid Y
+    result += details[columnIndex.PosX] + 'mm,'; // Ref X
+    result += details[columnIndex.PosY] + 'mm,'; // Ref Y
+    result += details[columnIndex.PosX] + 'mm,'; // Pad X
+    result += details[columnIndex.PosY] + 'mm,'; // Pad Y
+    result += details[columnIndex.Side] + ','; // Layer
+    result += details[columnIndex.Rot] + ','; // Rotation
+    result += details[columnIndex.Val] + ''; // Comment
+
+    return result;
 }
-
-function outputFooterdata(){
-	var result = '';
-	result += '\r\nPuzzle';
-	result += '\r\n76.47';
-	result += '\r\n31.78';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n74.03';
-	result += '\r\n119.11';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n110.00';
-	result += '\r\n110.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n-50.00';
-	result += '\r\n-50.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n73.92';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n30.15';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n72.82';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\n31.07';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n0.00';
-	result += '\r\n';
-	result += '\r\nOther';
-	result += '\r\n1.20';
-	result += '\r\n';
-	result += '\r\nTrue';
-	result += '\r\nTrue';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\n';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\nFalse';
-	result += '\r\n';
-	result += '\r\nMark';
-	result += '\r\n1';
-	result += '\r\n0';
-	result += '\r\nFalse';
-	result += '\r\n11';
-	result += '\r\n';
-	return result;
-}
-
-
-
